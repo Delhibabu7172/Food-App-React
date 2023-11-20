e880cccbafc5248c6c2e4635d85f559f5b7921d9
Delhi Babu <rldb143@gmail.com>
2023-11-20 08:33:05 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 <h1 className='pl-4  text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-400'>Best</span></h1>
                 <h1 className='pl-4  text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>Foods <span className='text-orange-400'>Delivered <br/> Here...</span></h1>
             </div>        
-               <img className='w-full max-h-[500px] object-cover' src={image1} alt="" />
+               <img className='w-full max-h-[500px] object-cover' src="https://images.unsplash.com/photo-1534080564583-6be75777b70a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt={image1} />
         </div>
     </div>
   )
